9036e036157a6e563e6bf042899236e8860d79db
midorikawaryo <ryo.midorikawa@drecom.co.jp>
2021-01-02 10:52:13 +0900

    [add]Google Analyticsを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,29 @@
 import '../styles/global.css'
 import 'katex/dist/katex.css'
 import Footer from '../components/footer'
+import * as gtag from '../lib/gtag'
+import React, { useEffect } from 'react'
+import { Router } from 'next/router'
 
-export default ({ Component, pageProps }) => (
-  <>
-    <Component {...pageProps} />
-    <Footer />
-  </>
-)
+export default ({ Component, pageProps }) => {
+  useEffect(() => {
+    if (!gtag.existsGaId) {
+      return
+    }
+
+    const handleRouteChange = (path: any) => {
+      gtag.pageview(path)
+    }
+
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
+  return (
+    <React.Fragment>
+      <Component {...pageProps} /> <Footer />
+    </React.Fragment>
+  )
+}
