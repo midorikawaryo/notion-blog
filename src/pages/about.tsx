329fa0ff2e311a1325456edb2cfd6e3507815ffa
midori <ryo.midorikawa@drecom.co.jp>
2021-01-14 23:10:16 +0900

    [add]プロフィールを追加

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,7 +7,13 @@ export default () => (
     <Header titlePre="プロフィール" />
     <div className={sharedStyles.layout}>
       <h1>プロフィール</h1>
-      <h2>comming soon...</h2>
+      <img src="/prof.png" alt="プロフィール画像" />
+      <h2>ろい</h2>
+      <div className="explanation">
+        <p>
+          HR業界で営業を経験し、現在はサーバーサイドエンジニアをやっています。
+        </p>
+      </div>
     </div>
   </>
 )
